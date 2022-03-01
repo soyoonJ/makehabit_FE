f147d87a5022505e1888dc0faa7cf17e2472a891
dokyunglee0905 <97431034+dokyunglee0905@users.noreply.github.com>
2022-03-02 00:30:16 +0900

    page 추가
    page 추가

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,15 +1,12 @@
 import React from "react";
 import styled from "styled-components";
 
-
-const Main = ()=>{
-    return (
-        <React.Fragment>
-            <div>메인페이지인디유!</div>
-        </React.Fragment>
-    );
-}
-
-
+const Main = () => {
+  return (
+    <React.Fragment>
+      <div>메인페이지인디유!</div>
+    </React.Fragment>
+  );
+};
 
 export default Main;
