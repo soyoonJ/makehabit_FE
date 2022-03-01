d0dab87731bb94bd2e35d99d4243fa386f163cd8
dokyunglee0905 <97431034+dokyunglee0905@users.noreply.github.com>
2022-03-02 01:04:19 +0900

    Main page_도경
    Main page_도경

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,12 +1,19 @@
 import React from "react";
 import styled from "styled-components";
+import { Button, Input, Text, Image } from "../elements";
 
 const Main = () => {
   return (
     <React.Fragment>
-      <div>메인페이지인디유!</div>
+      <Header>
+        <Image src="imges/logoEx.png" alt=""></Image>
+      </Header>
     </React.Fragment>
   );
 };
+
+const Header = styled.div`
+  margin: 5% 3%;
+`;
 
 export default Main;
