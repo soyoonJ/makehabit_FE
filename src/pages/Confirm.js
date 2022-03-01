4ec2e0fc0c8c63089b2b81381a3dcf810b867e2a
Soyoon Jeong <96245651+soyoonJ@users.noreply.github.com>
2022-03-02 02:41:04 +0900

    Confirm.js 레이아웃 세팅

--- src/pages/Confirm.js
+++ src/pages/Confirm.js
@@ -1,7 +1,147 @@
 import React from "react";
 
+import { Grid, Text, Input, Image } from "../elements";
+
+import styled from "styled-components";
+
 const Confirm = () => {
-  return <div></div>;
+  const [modalOpen, setModalOpen] = React.useState(false);
+  const openModal = () => {
+    setModalOpen(true);
+  };
+
+  const closeModal = () => {
+    setModalOpen(false);
+  };
+
+  return (
+    <React.Fragment>
+      <Grid padding="16px" position="relative">
+        <ConfirmText>인증하기</ConfirmText>
+        <Title>뷰 페이지 다 만들기</Title>
+        <SubTitle>
+          오늘의 도전을 성공하신 oo님! 인증사진을 올리고 포인트?
+        </SubTitle>
+
+        <input type="file"></input>
+        <Image shape="rectangle"></Image>
+
+        <Example>
+          챌린지 인증 예시가 궁금하다면?
+          <span
+            style={{
+              cursor: "pointer",
+            }}
+            onClick={openModal}
+          >
+            [아이콘]
+          </span>
+        </Example>
+
+        <CommentTitle>코멘트</CommentTitle>
+        <div>예쁘게 어쩌구~~</div>
+        <Textarea rows="8"></Textarea>
+      </Grid>
+
+      <Button>인증완료하기</Button>
+
+      {modalOpen ? (
+        <Modal>
+          <section>
+            <div onClick={closeModal}>X</div>
+            <Grid padding="30px 30px 0px 30px">
+              <div>챌린지 인증예시</div>
+              <div>
+                호스트가 챌린지 개설 시 작성한 인증방법 호스트가 챌린지 개설 시
+                작성한 인증방법 호스트가 챌린지 개설 시 작성한 인증방법 호스트가
+                챌린지 개설 시 작성한 인증방법 호스트가 챌린지 개설 시 작성한
+                인증방법 호스트가 챌린지 개설 시 작성한 인증방법
+              </div>
+            </Grid>
+          </section>
+        </Modal>
+      ) : null}
+    </React.Fragment>
+  );
 };
+
+// 인증하기 텍스트
+const ConfirmText = styled.div`
+  text-align: center;
+  font-weight: bold;
+  font-size: 20px;
+  margin-bottom: 30px;
+`;
+
+// 타이틀
+const Title = styled.div`
+  font-weight: bold;
+  font-size: 15px;
+`;
+const SubTitle = styled.div`
+  margin-bottom: 10px;
+`;
+
+// 챌린지 예시 도움말
+const Example = styled.div`
+  text-align: right;
+`;
+
+// 코멘트 작성란
+const CommentTitle = styled.div`
+  margin-top: 20px;
+`;
+const Textarea = styled.textarea`
+  width: 100%;
+  resize: none;
+`;
+const Button = styled.button`
+  position: absolute;
+  width: 100%;
+  height: 80px;
+  border: none;
+  position: fixed;
+  bottom: 0px;
+  color: white;
+  background: orange;
+`;
+
+const Modal = styled.div`
+  position: absolute;
+  top: 0px;
+  right: 0px;
+  bottom: 0px;
+  left: 0px;
+  background: rgba(0, 0, 0, 0.6);
+  z-index: 99;
+  display: flex;
+
+  section {
+    width: 80%;
+    height: 50%;
+    margin: auto;
+    align-self: center;
+    background: #fff;
+    border-radius: 10px;
+  }
+
+  section > div {
+    &:nth-child(1) {
+      cursor: pointer;
+      text-align: right;
+      margin: 20px;
+      font-size: 1.3rem;
+    }
+  }
+
+  section > div > div {
+    &:nth-child(1) {
+      font-weight: bold;
+      margin-bottom: 20px;
+    }
+    &:nth-child(2) {
+    }
+  }
+`;
 
 export default Confirm;
--- src/pages/MyChallenge.js
+++ src/pages/MyChallenge.js
@@ -4,19 +4,7 @@ import React from "react";
 const MyChallenge = () => {
   return (
     <React.Fragment>
-      <div>
-        <div>인증하기</div>
-        <div>타이틀</div>
-        <div>오늘의 도전을 성공하신 oo님! 인증사진을 올리고 포인트?</div>
-
-        <div>챌린지 인증 예시가 궁금하다면?</div>
-
-        <div>코멘트</div>
-        <div>예쁘게 어쩌구~~</div>
-        <textarea></textarea>
-
-        <button>인증완료하기</button>
-      </div>
+      <div></div>
     </React.Fragment>
   );
 };
