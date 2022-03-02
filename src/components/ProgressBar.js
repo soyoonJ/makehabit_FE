c96984a6ec74d4a41011e813898b86e389cc4b04
Bong <borobong230@gmail.com>
2022-03-02 21:10:15 +0900

    마이페이지 추가

---
+++ src/components/ProgressBar.js
@@ -0,0 +1,44 @@
+import styled from "styled-components";
+import { useSelector } from "react-redux";
+
+function Progressbar() {
+  //   const quizList = useSelector((state) => state.quiz.quizList);
+  //   const answer = useSelector((state) => state.quiz.answer);
+
+  return (
+    <Container>
+      <Progress width={(5 / 10) * 100 + "%"} />
+      <Dot />
+    </Container>
+  );
+}
+
+export default Progressbar;
+
+const Container = styled.div`
+  margin: 50px auto;
+  background-color: #eee;
+  width: 500px;
+  height: 20px;
+  display: flex;
+  align-items: center;
+  border-radius: 20px;
+`;
+const Progress = styled.div`
+  background-color: #000;
+  width: ${(props) => props.width};
+  height: 100%;
+  transition: width 1s;
+  border-radius: 20px;
+`;
+
+//프로그레스 바에 원 달아서 프로그레스 바가 차오를 때 같이 차오름
+const Dot = styled.div`
+  width: 40px;
+  height: 40px;
+  box-sizing: border-box;
+  border: 5px solid #000;
+  border-radius: 20px;
+  background: #fff;
+  margin-left: -10px;
+`;
--- src/pages/Mypage.js
+++ src/pages/Mypage.js
@@ -1,7 +1,81 @@
-import React from "react";
+import React, { useState } from "react";
 
+import { Grid, Text, Input, Button } from "../elements";
+
+import ProgressBar from "../components/ProgressBar";
+import NicknameModal from "../components/NicknameModal";
+
+import { HiOutlinePencil } from "react-icons/hi";
 const Mypage = () => {
-  return <div></div>;
+  //닉네임 변경 모달 값 가져오기 (자식(CategoryModal) -> 부모(postWrite))
+  const [nicknameValue, setNicknameValue] = useState("");
+
+  const getData = (nicknameValue) => {
+    setNicknameValue(nicknameValue);
+  };
+
+  //카테고리 팝업
+  let [modalopen, setModalopen] = React.useState(false);
+  //카테고리 팝업 열기
+  const openModal = () => {
+    setModalopen(true);
+  };
+
+  //카테고리 팝업 닫기
+  const closeModal = () => {
+    setModalopen(false);
+  };
+  return (
+    <Grid>
+      <Grid is_flex justifyContent="center" padding="100px 0 0 0">
+        {/* <canvas width="150px" height="150px"> */}
+        <img
+          src="https://media.istockphoto.com/vectors/man-face-character-people-work-profile-image-vector-id931879564"
+          width="150px"
+          height="150px"
+          alt=""
+        />
+        {/* </canvas> */}
+      </Grid>
+      {/* 닉네임 / 닉네임 변경 */}
+      <Grid is_flex textAlign="center" justifyContent="center" padding="2% 5%">
+        <Button _onClick={openModal}>
+          닉네임 &nbsp;&nbsp;&nbsp;
+          <HiOutlinePencil />
+        </Button>
+        <NicknameModal
+          open={modalopen}
+          close={closeModal}
+          getData={getData}
+        ></NicknameModal>
+      </Grid>
+
+      {/* 레벨 / 남은 경험치*/}
+      <Grid is_flex textAlign="center">
+        <Text>Lv.1</Text>
+        <Text>다음 레벨까지 100경험치</Text>
+      </Grid>
+      {/* 경험치 바 */}
+      <Grid is_flex justifyContent="center" padding="5%">
+        <ProgressBar />
+      </Grid>
+      <Grid padding="2% 5%">
+        <Button>내 캐릭터 꾸미기</Button>
+      </Grid>
+      <Grid padding="2% 5%">
+        <Button>좋아요 모아보기</Button>
+      </Grid>
+      <Grid padding="2% 5%">
+        <Button>문의 FAQ</Button>
+      </Grid>
+      <Grid padding="2% 5%">
+        <Button>나의 기록보기</Button>
+      </Grid>
+      <Grid padding="2% 5%">
+        <Button>로그아웃</Button>
+      </Grid>
+    </Grid>
+  );
 };
 
 export default Mypage;
