aa723c930888ef0ecdca1cebad9c6d36f5b3ed4d
Bong <borobong230@gmail.com>
2022-03-04 22:26:16 +0900

    post.js 작업 추가

--- src/redux/modules/post.js
+++ src/redux/modules/post.js
@@ -1,32 +1,152 @@
 // 액션 만들어주는 것들
 import { createAction, handleActions } from "redux-actions";
 import { produce } from "immer";
+import { apis } from "../../shared/Api";
 
 // actions
-const SET_TAB = "SET_TAB";
-
-// action creators
-const setTab = createAction(SET_TAB, (page) => ({ page }));
+const ADD_POST = "ADD_POST";
+const EDIT_POST = "EDIT_POST";
+const DELETE_POST = "DELETE_POST";
 
 // initialState
 const initialState = {
   page: null,
 };
 
+//게시물 등록
+const addPostDB = (
+  title,
+  category,
+  thumnail,
+  startAt,
+  content,
+  howtoContent,
+  tag
+) => {
+  return function (dispatch, useState, { history }) {
+    apis
+      .createChallenge(
+        title,
+        category,
+        thumnail,
+        startAt,
+        content,
+        howtoContent,
+        tag
+      )
+      .then((response) => {
+        console.log("게시물 등록");
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  };
+};
+
+//이미지 업로드
+const uploadImageDB = (challengeId, imgUrl, challengeTitle, comment) => {
+  return function (dispatch, useState, { history }) {
+    apis
+      .confirm(challengeId, imgUrl, challengeTitle, comment)
+      .then((response) => {
+        console.log("이미지 업로드");
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  };
+};
+
+//상세페이지 불러오기
+const getDetailPostDB = (challengeId) => {
+  return function (dispatch, getState, { history }) {
+    apis
+      .detail(challengeId)
+      .then((response) => {
+        console.log("상세페이지");
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  };
+};
+
+//참여하기
+const joinDB = (challengId) => {
+  return function (dispatch, getState, { history }) {
+    apis
+      .join(challengId)
+      .then((response) => {
+        console.log("참여하기");
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  };
+};
+
+//참여취소하기
+const joinCancelDB = (challengId) => {
+  return function (dispatch, getState, { history }) {
+    apis
+      .joinCancel(challengId)
+      .then((response) => {
+        console.log("참여취소하기");
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  };
+};
+
+//찜하기
+const likeDB = (challengId) => {
+  return function (dispatch, getState, { history }) {
+    apis
+      .like(challengId)
+      .then((response) => {
+        console.log("찜하기");
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  };
+};
+
+//찜하기 취소하기
+const dislikeDB = (challengId) => {
+  return function (dispatch, getState, { history }) {
+    apis
+      .dislike(challengId)
+      .then((response) => {
+        console.log("찜하기");
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  };
+};
+
 // redux
 export default handleActions(
   {
-    [SET_TAB]: (state, action) =>
-      produce(state, (draft) => {
-        // console.log(action.payload.page);
-        draft.page = action.payload.page;
-      }),
+    // [SET_TAB]: (state, action) =>
+    //   produce(state, (draft) => {
+    //     // console.log(action.payload.page);
+    //     draft.page = action.payload.page;
+    //   }),
   },
   initialState
 );
 
 const actionCreators = {
-  setTab,
+  addPostDB,
+  uploadImageDB,
+  getDetailPostDB,
+  joinDB,
+  joinCancelDB,
+  likeDB,
+  dislikeDB,
 };
 
 export { actionCreators };
