d3ad84fe261921f6a83e5b896269dbc85cece611
Soyoon Jeong <96245651+soyoonJ@users.noreply.github.com>
2022-03-08 03:40:19 +0900

    글꼴 추가, 개설완료페이지 틀 작업

---
+++ src/pages/CompleteOpen.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const CompleteOpen = (props) => {
+  return (
+    <React.Fragment>
+      <div>개설완료페이지</div>
+    </React.Fragment>
+  );
+};
+
+export default CompleteOpen;
