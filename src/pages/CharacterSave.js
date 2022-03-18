3f03ef21924133542241df44173e1c7227aeccd8
Soyoon Jeong <96245651+soyoonJ@users.noreply.github.com>
2022-03-18 23:15:39 +0900

    CharacterSave 페이지 구현 완료

--- src/pages/CharacterSave.js
+++ src/pages/CharacterSave.js
@@ -1,120 +1,150 @@
 import React from "react";
 
-import { ContainerGrid, Grid, Text, Input, Image, Button } from "../elements";
 import { history } from "../redux/configureStore";
+import { useDispatch, useSelector } from "react-redux";
+
+import { actionCreators as characterActions } from "../redux/modules/character";
+import { ContainerGrid, Grid, Text, Input, Image, Button } from "../elements";
+import { ReactComponent as CheckImg } from "../img/icon_check.svg";
+
 import styled from "styled-components";
 
 const CharacterSave = () => {
+  const dispatch = useDispatch();
+
+  React.useEffect(() => {
+    dispatch(characterActions.getItemDB());
+
+    console.log("오냐냐냐냐냐");
+  }, []);
+
+  const isEquip = useSelector((state) => state.character?.isEquip);
+  const equipColor = isEquip?.find((e) => e.category === "color");
+  const equipBg = isEquip?.find((e) => e.category === "background");
+  const equipClothes = isEquip?.find((e) => e.category === "clothes");
+  const equipAcc = isEquip?.find((e) => e.category === "acc");
+  const equipEmotion = isEquip?.find((e) => e.category === "emotion");
+
+  const Item = process.env.PUBLIC_URL + "/items/large";
+
   return (
     <React.Fragment>
       <ContainerGrid>
-        <TopBox>
-          <div>
-            <img
-              src={
-                process.env.PUBLIC_URL + "/images/illust_exercise_samkki.png"
-              }
-              alt="상단 캐릭터 일러스트"
-              style={{ height: "26.77vh" }}
-            />
-          </div>
-          <div>캐릭터 저장 완료!</div>
-        </TopBox>
+        <CharacterWrap>
+          <ImgContainer>
+            <ItemImg src={Item + equipBg?.itemImgUrl} />
+            <ItemImg src={Item + equipColor?.itemImgUrl} />
+            <ItemImg src={Item + equipClothes?.itemImgUrl} />
+            <ItemImg src={Item + equipAcc?.itemImgUrl} />
+            <ItemImg src={Item + equipEmotion?.itemImgUrl} />
+          </ImgContainer>
+        </CharacterWrap>
 
-        <Button
-          position="absolute"
-          right="0"
-          left="0"
-          bottom="0"
-          fontSize="2.6vh"
-          fontWeight="bold"
-          height="7.93vh"
-          width="90%"
-          maxWidth="380px"
-          margin="0 auto 2.36vh auto"
-          bg="rgba(255, 139, 55, 1)"
-          _onClick={() => {
-            history.push("/mypage");
+        <Circle>
+          <CheckImg fill="#6825D6" width="4.03vh" height="4.03vh" />
+        </Circle>
+        <h2
+          style={{
+            textAlign: "center",
+            fontSize: "2.6vh",
+            lineHeight: "3.414vh",
+            marginBottom: "2.6vh",
           }}
         >
-          확인
-        </Button>
+          내 캐릭터가 저장되었어요!
+        </h2>
+        <div
+          style={{
+            textAlign: "center",
+            color: "#707070",
+            fontSize: "1.66vh",
+            lineHeight: "2.65vh",
+          }}
+        >
+          꾸준히 챌린지에 도전하며 포인트를 모아
+          <br />
+          다양한 아이템으로 나를 꾸며봐요!
+        </div>
+
+        <Buttons>
+          <Button
+            fontSize="2.6vh"
+            fontWeight="bold"
+            height="7.93vh"
+            bg="rgba(255, 139, 55, 1)"
+            margin="0 0 1.89vh 0"
+            _onClick={() => {
+              history.push("/mypage");
+            }}
+          >
+            확인
+          </Button>
+          <Button
+            borderRadius="5px"
+            border="1px solid #FF8B37"
+            fontSize="2.13vh"
+            fontWeight="bold"
+            height="6.16vh"
+            bg="#fff"
+            color="rgba(255, 139, 55, 1)"
+            _onClick={() => {}}
+          >
+            내 캐릭터 공유하기
+          </Button>
+        </Buttons>
       </ContainerGrid>
     </React.Fragment>
   );
 };
 
-const TopBox = styled.div`
-  margin: 0 1.5rem 5.56vh 1.5rem;
-  padding-top: 8.05vh;
+// 개설/참여완료 주황색 info 박스
+const CharacterWrap = styled.div`
   display: flex;
   flex-direction: column;
-  align-items: center;
-
-  & > div {
-    &:nth-child(1) {
-      margin-bottom: 3.43vh;
-    }
-
-    &:nth-child(2) {
-      font-size: 2.6vh;
-      font-weight: bold;
-      margin-bottom: 0.75rem;
-      text-align: center;
-    }
-
-    &:nth-child(3) {
-      font-size: 2.36vh;
-      text-align: center;
-      margin-bottom: 0.5rem;
-    }
-
-    &:nth-child(4) {
-      font-size: 2.36vh;
-      text-align: center;
-    }
+  position: relative;
+  height: 46vh;
+  margin-bottom: 4.97vh;
+  // 모바일 버전
+  @media (min-width: 420px) {
+    height: 48.5vh;
   }
 `;
 
-// 개설/참여완료 주황색 info 박스
-const InfoBox = styled.div`
+const ImgContainer = styled.div`
   width: 100%;
-  background: #fff1e7;
-  border-radius: 5px;
+  height: 48.5vh;
+  position: relative;
   display: flex;
   justify-content: center;
-  text-align: left;
-`;
-const InfoText = styled.div`
-  padding: 1.813rem 2.25rem;
-  font-size: 2.36vh;
-  letter-spacing: -0.5%;
-  line-height: 3.31vh;
-
-  & > div {
-    &:nth-child(1),
-    &:nth-child(2) {
-      margin-bottom: 0.7vh;
-    }
-  }
 `;
 
-// 인증완료 보상 info 박스
-const GiftBox = styled.div`
-  width: 100%;
-  background: #fff1e7;
-  border-radius: 5px;
+const ItemImg = styled.img`
+  height: 100%;
+  position: absolute;
+  z-index: 1;
+`;
+
+const Circle = styled.div`
+  width: 7.11vh;
+  height: 7.11vh;
+  border: 3px solid #6825d6;
+  border-radius: 50%;
+
   display: flex;
   justify-content: center;
   align-items: center;
-  margin-bottom: 1.188rem;
+
+  margin: 0 auto 2.96vh auto;
 `;
-const Content = styled.div`
-  font-size: 2.96vh;
-  font-weight: bold;
-  display: flex;
-  align-items: center;
-  margin: 3.79vh;
+
+const Buttons = styled.div`
+  position: absolute;
+  right: 0;
+  left: 0;
+  bottom: 0;
+  width: 90%;
+  max-width: 380px;
+  margin: 0 auto 2.36vh auto;
 `;
 
 export default CharacterSave;
