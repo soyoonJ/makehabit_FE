11ff4ac0990ddc8c8c8818515553facb8a3f1e94
borobong2 <borobong230@gmail.com>
2022-07-18 20:28:16 +0900

    refactor: Migrate js to ts

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -6,6 +6,7 @@ import { history } from "../redux/configureStore";
 
 import { ReactComponent as LeftIcon } from "../img/icon_left.svg";
 import { ReactComponent as RightIcon } from "../img/icon_right.svg";
+import { render } from "@testing-library/react";
 
 const Banner = () => {
   const [bannerIndex, setBannerIndex] = React.useState<number>(0);
@@ -58,7 +59,6 @@ const Banner = () => {
     );
     return () => clearInterval(slider);
   }, []);
-
   return (
     <Container>
       <LeftIcon
@@ -70,8 +70,8 @@ const Banner = () => {
           zIndex: "3",
         }}
         width="20"
-        heigth="20"
-        alt="left icon"
+        height="20"
+        // alt="left icon"
         fill="#000"
       />
       <Carousel bannerIndex={bannerIndex}>
@@ -126,8 +126,8 @@ const Banner = () => {
           zIndex: "3",
         }}
         width="20"
-        heigth="20"
-        alt="right icon"
+        height="20"
+        // alt="right icon"
         fill="#000"
       />
     </Container>
@@ -152,7 +152,7 @@ const Container = styled.div`
   overflow-x: hidden;
   position: relative;
 `;
-const Carousel = styled.div`
+const Carousel = styled.div<{ bannerIndex: number }>`
   display: flex;
 
   @media only screen and (min-width: 420px) {
@@ -173,7 +173,7 @@ const Carousel = styled.div`
 
   transition: all 0.2s;
 `;
-const ContentBox = styled.div`
+const ContentBox = styled.div<{ index: number }>`
   font-size: 22px;
   height: 175px;
   font-weight: 700;
@@ -209,7 +209,7 @@ const Circle = styled.div`
   cursor: pointer;
 `;
 
-const Img = styled.div`
+const Img = styled.div<{ imgURL?: string; index?: number }>`
   background-image: url(${(props) => props.imgURL});
 
   @media (min-width: 420px) {
@@ -223,12 +223,5 @@ const Img = styled.div`
   background-position: center;
   background-repeat: no-repeat;
   z-index: 2;
-`;
-const IconImg = styled.img`
-  position: absolute;
-  cursor: pointer;
-  z-index: 3;
-  width: 18px;
-  height: 18px;
 `;
 export default Banner;
