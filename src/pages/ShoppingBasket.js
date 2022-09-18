8eb71de642a5f3a012fcef613aae3885c68beeea
borobong2 <borobong230@gmail.com>
2022-09-18 10:37:29 +0900

    test

--- src/pages/ShoppingBasket.js
+++ src/pages/ShoppingBasket.js
@@ -1,7 +1,7 @@
-import React from 'react';
+import React from "react";
 
-import PurchaseCharacter from '../components/PurchaseCharacter';
-import ShoppingBasketModal from '../components/ShoppingBasketModal';
+import PurchaseCharacter from "../components/PurchaseCharacter";
+import ShoppingBasketModal from "../components/ShoppingBasketModal";
 
 const ShoppingBasket = () => {
 	return (
