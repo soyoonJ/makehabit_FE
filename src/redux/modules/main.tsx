2337bbad660ab4315ddb52346bd088833e729c3e
Soyoon Jeong <96245651+soyoonJ@users.noreply.github.com>
2022-10-28 00:31:40 +0900

    refactor: main.js to main.tsx

--- src/redux/modules/main.tsx
+++ src/redux/modules/main.tsx
@@ -11,24 +11,17 @@ const GET_NEW = "GET_NEW";
 const GET_STUDY = "GET_STUDY";
 
 // action creators
-const getSearch = createAction(GET_SEARCH, (searchWord_list) => ({
-  searchWord_list,
+const getSearch = createAction(GET_SEARCH, (search_data) => ({
+  search_data,
 }));
 
-const getCategory = createAction(
-  GET_CATEGOTY,
-  (category_list, category, checkLoadAll) => ({
-    category_list,
-    category,
-    checkLoadAll,
-  })
-);
-const newCategory = createAction(GET_NEW, (new_list) => ({
-  new_list,
+const getCategory = createAction(GET_CATEGOTY,(category_data) => ({category_data}));
+const newCategory = createAction(GET_NEW, (new_list_data) => ({
+  new_list_data,
 }));
 
-const studyCategory = createAction(GET_STUDY, (study_list) => ({
-  study_list,
+const studyCategory = createAction(GET_STUDY, (study_list_data) => ({
+  study_list_data,
 }));
 
 const getRecommend = createAction(GET_RECOMMEND, (recommend_list) => ({
@@ -43,6 +36,11 @@ const initialState = {
   recommend_list: [],
   new_list: [],
   study_list: [],
+
+  search_data: {challenges:[]},
+  category_data: {challenges:[]},
+  new_list_data: {challenges:[]},
+  study_list_data: {challenges:[]},
 };
 
 // 미들웨어
@@ -136,14 +134,12 @@ export default handleActions(
   {
     [GET_SEARCH]: (state, action) =>
       produce(state, (draft) => {
-        draft.searchWord_list = action.payload.searchWord_list.challenges;
+        draft.searchWord_list = action.payload.search_data.challenges;
       }),
 
     [GET_CATEGOTY]: (state, action) =>
       produce(state, (draft) => {
-        draft.category_list = action.payload.category_list.challenges;
-
-        draft.checkLoadAll = action.payload.checkLoadAll;
+        draft.category_list = action.payload.category_data.challenges;
       }),
 
     [GET_RECOMMEND]: (state, action) =>
@@ -153,13 +149,13 @@ export default handleActions(
 
     [GET_NEW]: (state, action) =>
       produce(state, (draft) => {
-        draft.new_list = action.payload.new_list.challenges;
+        draft.new_list = action.payload.new_list_data.challenges;
       }),
 
     [GET_STUDY]: (state, action) =>
       produce(state, (draft) => {
         // console.log("카테고리 목록 조회", action.payload);
-        draft.study_list = action.payload.study_list.challenges;
+        draft.study_list = action.payload.study_list_data.challenges;
       }),
   },
 
